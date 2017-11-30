f93b7c7bb688e03fcd839d148c2400bc0b00c291
Artur Lachor <a.lachor96@gmail.com>
2017-12-01 00:03:31 +0100

    Added blob shadow projectors and ProBuilder to the project.

---
+++ Assets/_Scripts/GroundCheck.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundCheck : MonoBehaviour
+{
+    private PlayerController player;
+
+	void Start ()
+    {
+        player = gameObject.GetComponentInParent<PlayerController>();
+	}
+
+    void OnTriggerEnter()
+    {
+        player.grounded = true;
+    }
+
+    void OnTriggerStay()
+    {
+        player.grounded = true;
+    }
+
+    void OnTriggerExit()
+    {
+        player.grounded = false;
+    }
+
+}
--- Assets/_Scripts/PlayerController.cs
+++ Assets/_Scripts/PlayerController.cs
@@ -7,8 +7,11 @@ public class PlayerController : MonoBehaviour
 
     private Rigidbody rb;
 
-    public float movementSpeed;
+    public bool grounded;
 
+    public float movementSpeed;
+    public float jumpForce;
+    public float maxSpeed;
 	void Start ()
     {
         rb = gameObject.GetComponent<Rigidbody>();
@@ -26,9 +29,46 @@ public class PlayerController : MonoBehaviour
         Vector3 movement = new Vector3(horizontal, 0f, vertical);
         movement = Quaternion.AngleAxis(-135, Vector3.up) * movement;
 
-        Debug.Log(movement.x);
-        Debug.Log(movement.z);
+        
+        //Constant input
+        if(movement.magnitude > 1f)
+        {
+            movement = movement.normalized;
+        }
+        
+        //Speed limiting
+        if(rb.velocity.x > maxSpeed)
+        {
+            rb.velocity = new Vector3(maxSpeed, rb.velocity.y, rb.velocity.z);
+        }
+
+        if (rb.velocity.x < -maxSpeed)
+        {
+            rb.velocity = new Vector3(-maxSpeed, rb.velocity.y, rb.velocity.z);
+        }
+
+        if (rb.velocity.z > maxSpeed)
+        {
+            rb.velocity = new Vector3(rb.velocity.x, rb.velocity.y, maxSpeed);
+        }
+
+        if (rb.velocity.z < -maxSpeed)
+        {
+            rb.velocity = new Vector3(rb.velocity.x, rb.velocity.y, -maxSpeed);
+        }
+
+        Debug.Log("X axis: " + movement.x);
+        Debug.Log("Z axis: " + movement.z);
+
+        Debug.Log("Magnitude: " + movement.magnitude);
 
         rb.AddForce(movement * movementSpeed);
+
+        if(Input.GetButtonDown("Jump") && grounded)
+        {
+            rb.AddForce(0, jumpForce, 0);
+        }
+
+        
     }
 }
