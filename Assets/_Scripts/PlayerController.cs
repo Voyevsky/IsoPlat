e36fc353bd841130e593133ee158d0180c664e86
Artur Lachor <a.lachor96@gmail.com>
2017-12-16 21:38:17 +0100

    Fixed accidental double jumps & double dashes

--- Assets/_Scripts/PlayerController.cs
+++ Assets/_Scripts/PlayerController.cs
@@ -12,6 +12,11 @@ public class PlayerController : MonoBehaviour
     public float movementSpeed;
     public float jumpForce;
     public float maxSpeed;
+    public float dashSpeed;
+
+    private Vector3 direction = new Vector3(0f, 0f, 0f);
+    private bool isMoving = false;
+
 	void Start ()
     {
         rb = gameObject.GetComponent<Rigidbody>();
@@ -19,56 +24,57 @@ public class PlayerController : MonoBehaviour
 	
 	void FixedUpdate ()
     {
-        //Basic movement
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
 
-        Debug.Log(horizontal);
-        Debug.Log(vertical);
+        //Fake friction
+        Vector3 fakeFriction = rb.velocity;
+        fakeFriction.y = rb.velocity.y;
+        fakeFriction.z *= 0.5f;
+        fakeFriction.x *= 0.5f;
+
+        rb.velocity = fakeFriction;
+
+        //Basic movement
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        
 
         Vector3 movement = new Vector3(horizontal, 0f, vertical);
         movement = Quaternion.AngleAxis(-135, Vector3.up) * movement;
 
         
         //Constant input
-        if(movement.magnitude > 1f)
+        if(movement.magnitude > 0)
         {
+            isMoving = true;
             movement = movement.normalized;
         }
-        
-        //Speed limiting
-        if(rb.velocity.x > maxSpeed)
+        else
         {
-            rb.velocity = new Vector3(maxSpeed, rb.velocity.y, rb.velocity.z);
+            isMoving = false;
         }
 
-        if (rb.velocity.x < -maxSpeed)
-        {
-            rb.velocity = new Vector3(-maxSpeed, rb.velocity.y, rb.velocity.z);
-        }
-
-        if (rb.velocity.z > maxSpeed)
-        {
-            rb.velocity = new Vector3(rb.velocity.x, rb.velocity.y, maxSpeed);
-        }
-
-        if (rb.velocity.z < -maxSpeed)
-        {
-            rb.velocity = new Vector3(rb.velocity.x, rb.velocity.y, -maxSpeed);
-        }
-
-        Debug.Log("X axis: " + movement.x);
-        Debug.Log("Z axis: " + movement.z);
-
-        Debug.Log("Magnitude: " + movement.magnitude);
+        direction = movement.normalized;
 
         rb.AddForce(movement * movementSpeed);
+        
+    }
 
-        if(Input.GetButtonDown("Jump") && grounded)
+    void Update()
+    {
+        //Dashing
+
+        if (Input.GetButtonDown("Dash") && isMoving)
+        {
+            rb.AddForce(direction * dashSpeed);
+            Debug.Log("Dash!");
+        }
+        
+        //Jumping
+        if (Input.GetButtonDown("Jump") && grounded)
         {
             rb.AddForce(0, jumpForce, 0);
+            //grounded = false;
+            Debug.Log("Jump!");
         }
-
-        
     }
 }
